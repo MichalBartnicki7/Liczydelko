58b3775fb9268ab521a68d39c200c33e5974fbd7
MichalBartnicki7 <93383409+MichalBartnicki7@users.noreply.github.com>
2021-10-29 13:59:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Liczydelko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("To moj projekt Liczydelko");
+        }
+    }
+}
